09e6661a5c28c48eeade910069f94326b99ada73
Clif <cliftonritchie@gmail.com>
2020-02-23 15:25:33 -0600

    documentation

--- src/image_downloader.py
+++ src/image_downloader.py
@@ -5,13 +5,26 @@ dir_path = os.getcwd()
 
 
 class ImageDownloader:
+    """
+    Wrapper to pull a Google Image per search term into an output dir
+
+    Args:
+        queries (list): Search terms to be fed into Google Images
+        output_dir (string): Path to save downloaded images
+    """
+
     def __init__(self, queries, output_dir):
+        self.queries = queries
         self.output_dir = output_dir
         self.response = google_images_download.googleimagesdownload()
-        self.downloadimages(queries)
+        self.downloadimages()
 
-    def downloadimages(self, queries):
-        for query in queries:
+    def downloadimages(self):
+        """
+        Downloads one publicly usable and modifyable image from Google Images
+        per search term
+        """
+        for query in self.queries:
             arguments = {
                 "keywords": query,
                 "usage_rights": "labeled-for-reuse-with-modifications",
@@ -21,6 +34,6 @@ class ImageDownloader:
                 "print_urls": False,
                 "size": "medium",
                 "aspect_ratio": "wide",
-                "silent_mode": True
+                "silent_mode": True,
             }
             self.response.download(arguments)
